97decebe0298d30af4baa976ad6969498bf61099
Nick Eberhardt <eberhardt@Eberhardt-MacBook-Pro.local>
2018-10-17 09:57:15 -0700

    fix: titles and authors now showing

--- src/components/Book.js
+++ src/components/Book.js
@@ -29,8 +29,8 @@ render() {
                         </select>
                     </div>
                 </div>
-                <div className="book-title">{this.props.title}</div>
-                <div className="book-authors">{Array.isArray(this.props.authors) ? this.props.authors[0] : this.props.authors}</div>
+                <div className="book-title">{this.props.book.title}</div>
+                <div className="book-authors">{Array.isArray(this.props.book.authors) ? this.props.book.authors[0] : this.props.book.authors}</div>
             </div>
         </li> 
     );
